96ca7538bc6043d7538f7e954ce6ffc78b9d51f1
Said Al-Ghiyats <said.ghiyats@gmail.com>
2023-03-03 17:09:41 +0700

    hmmm

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -10,5 +10,25 @@ export type Surah = {
   };
   revelation: { arab: string; en: string; id: string };
   tafsir: { id: string };
-  verses: { text: { arab: string; transliteration: { en: string } } }[];
+  verses: {
+    number: {
+      inQuran: number;
+      inSurah: number;
+    };
+    meta: {
+      juz: number;
+      page: number;
+      manzil: number;
+      ruku: number;
+      hizbQuarter: number;
+      sajda: {
+        recommended: boolean;
+        obligatory: boolean;
+      };
+    };
+    text: { arab: string; transliteration: { en: string } };
+    translation: { en: string; id: string };
+    audio: { primary: string };
+    tafsir: { id: { short: string; long: string } };
+  }[];
 };
