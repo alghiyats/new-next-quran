9e7045180d8eb7cad6cc81c30b85a834a441bc50
Said Al-Ghiyats <said.ghiyats@gmail.com>
2023-03-05 17:39:52 +0700

    change

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -10,8 +10,8 @@ type Props = {
 const ListItem = ({ data }: Props) => {
   return (
     <Link href={`/surah/${data.name.transliteration.id.toLowerCase()}`}>
-      <div className="group pl-4 p-6 flex shadow-md hover:shadow-lg border dark:border-gray-500 duration-300 rounded-md dark:shadow-gray-500 justify-between dark:bg-slate-900">
-        <div className="flex items-center">
+      <div className="group pl-4 p-6 flex shadow-md hover:shadow-lg border dark:border-gray-500 duration-300 rounded-md dark:shadow-gray-500 justify-between dark:bg-slate-900 items-center">
+        <div className="flex ">
           <div className="flex justify-center items-center w-8 h-8 dark:bg-gray-700 bg-gray-100 mr-4 rounded-md text-xs">
             {data.number}
           </div>
