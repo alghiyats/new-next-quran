c49268bf1c68c9ceaf7120bf285d7213501afc23
Said Al-Ghiyats <said.ghiyats@gmail.com>
2023-03-06 18:21:28 +0700

    update

--- src/pages/surah/[id].tsx
+++ src/pages/surah/[id].tsx
@@ -36,7 +36,30 @@ const StaticPropsDetail = ({ item, errors }: Props) => {
           {`${item ? item.name.transliteration.id : 'Surah'} - Next Quran`}
         </title>
       </Head>
-      <ListDetail item={item} />
+      <div>
+        <div className="py-6 flex flex-col gap-4 shadow-md  border dark:border-gray-500 duration-300 rounded-md dark:shadow-gray-500 dark:bg-slate-900 mb-12">
+          <p className="font-bold text-xl text-center">
+            {item.name.transliteration.id}
+          </p>
+          <p className="text-center text-sm">
+            {item.numberOfVerses} Ayat - {item.revelation.id}
+          </p>
+          <p className=" font-['Uthmani'] text-3xl font-bold text-center">
+            {item.preBismillah !== null && item.preBismillah.text.arab}
+          </p>
+        </div>
+        <div className="flex gap-y-6 flex-col">
+          {item.verses.map((x) => (
+            <ListDetail
+              key={x.number.inSurah}
+              item={item}
+              arab={x.text.arab}
+              translation={x.translation.id}
+              ayat={x.number.inSurah}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
